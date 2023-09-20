8cf2bf1cbb9ecdd3ae3a584c1edd8226fb2c6c6b
inf <decimatedxd@gmail.com>
2023-09-20 10:13:06 +0200

    added text to product page

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -4,6 +4,10 @@ import React from "react";
 export default function page() {
   return (
     <>
+      <p className="text-center text-2xl font-semibold pb-8">
+        Here you can view or order the vintage cameras we have in our
+        collection.
+      </p>
       <Products />
     </>
   );
