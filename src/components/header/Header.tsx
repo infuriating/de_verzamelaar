4f80c7b12a3c056aeb1bfd09289fee3bb3298e13
infuriating <decimatedxd@gmail.com>
2023-09-28 12:03:25 +0200

    added cart item in the header if it exists

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import NavLogic from "./NavLogic";
 
 export default function Header() {
+  let cardProduct = null;
+
+  function setCardProduct() {
+    cardProduct = localStorage.getItem("product");
+  }
+
+  setCardProduct();
   return (
-    <div className="h-16 bg-card border-b mb-8 flex items-center justify-between">
-      <div className="w-[96%] ml-[2%] flex justify-between">
-        <Link href={"/"}>
-          <p className="font-semibold">FNS Cameras</p>
+    <>
+      {cardProduct && (
+        <Link href={`/cart/${cardProduct}`}>
+          <div className="px-2 py-[.1rem] bg-white text-black rounded-full absolute top-1 right-28 text-[10px]">
+            In cart: <span className="font-semibold">{cardProduct}</span>
+          </div>
         </Link>
-        <div className="flex gap-4">
-          <NavLogic />
+      )}
+      <div className="h-16 bg-card border-b mb-8 flex items-center justify-between">
+        <div className="w-[96%] ml-[2%] flex justify-between">
+          <Link href={"/"}>
+            <p className="font-semibold">FNS Cameras</p>
+          </Link>
+          <div className="flex gap-4">
+            <NavLogic />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
