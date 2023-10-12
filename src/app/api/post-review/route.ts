f9ed7b56c34c2849d85e69f8e6d07bcb5a5fdc94
inf <decimatedxd@gmail.com>
2023-10-12 14:15:11 +0200

    added functionality to post reviews

---
+++ src/app/api/post-review/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(request: NextResponse) {
+  const req = await request.json();
+
+  const review = await prisma.review.create({
+    data: {
+      name: req.name,
+      product: req.product,
+      title: req.title,
+      body: req.body,
+      rating: parseInt(req.rating),
+    },
+  });
+
+  return NextResponse.json(review);
+}
--- src/components/PostAReview.tsx
+++ src/components/PostAReview.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
 import { Separator } from "@/components/ui/separator";
 import {
@@ -14,63 +14,99 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import PostNewReview from "@/lib/PrismaFunctions/PostNewReview";
 
-export default function PostAReview() {
-  const [formData, setFormData] = useState({
-    name: "",
-    title: "",
-    body: "",
-    rating: 0,
-  });
+export default function PostAReview({ product }: any) {
+  const [productLoaded, setProductLoaded] = React.useState(false);
 
+  setTimeout(() => {
+    setProductLoaded(true);
+  }, 1500);
+
+  async function PostNewReview(e: any) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+
+    let name = data.name;
+    let product = data.product;
+    let title = data.title;
+    let body = data.body;
+    let rating = data.rating;
+
+    try {
+      await fetch("/api/post-review", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          product,
+          title,
+          body,
+          rating,
+        }),
+      }).then((res) => res.json());
+    } catch {
+      console.error("Something went wrong");
+    }
+
+    window.location.href = "/";
+  }
   return (
     <Card className="w-[500px]">
       <CardHeader>
         <CardTitle>Post a Review</CardTitle>
-        <CardDescription>Post a review for a product.</CardDescription>
+        <CardDescription>
+          Post a review for your previously ordered product.
+        </CardDescription>
       </CardHeader>
       <Separator />
       <CardContent className="p-3 flex flex-col">
-        <form>
+        <form onSubmit={PostNewReview}>
           <div className="pb-2">
             <Label className="text-lg font-semibold">Name</Label>
             <Input
+              name="name"
               className="rounded-[.25rem]"
               required
               placeholder="Name"
-              value={formData?.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
             />
           </div>
           <div className="pb-2">
+            <Label className="text-lg font-semibold">Product</Label>
+
+            {productLoaded ? (
+              <Input readOnly name="product" value={product} />
+            ) : (
+              <Input
+                disabled
+                readOnly
+                name="product"
+                placeholder="Retrieving product..."
+              />
+            )}
+          </div>
+
+          <div className="pb-2">
             <Label className="text-lg font-semibold">Title</Label>
             <Input
+              name="title"
               className="rounded-[.25rem]"
               required
               placeholder="Title"
-              value={formData?.title}
-              onChange={(e) =>
-                setFormData({ ...formData, title: e.target.value })
-              }
             />
           </div>
           <div className="pb-2">
             <Label className="text-lg font-semibold">Body</Label>
             <Input
+              name="body"
               className="rounded-[.25rem]"
               required
               placeholder="Body"
-              value={formData?.body}
-              onChange={(e) =>
-                setFormData({ ...formData, body: e.target.value })
-              }
             />
           </div>
           <Label className="text-lg font-semibold">Rating</Label>
-          <RadioGroup className="flex px-4 py-2 pb-4">
+          <RadioGroup required className="flex px-4 py-2 pb-4" name="rating">
             <RadioGroupItem value="1">1</RadioGroupItem>
             <RadioGroupItem value="2">2</RadioGroupItem>
             <RadioGroupItem value="3">3</RadioGroupItem>
@@ -78,7 +114,6 @@ export default function PostAReview() {
             <RadioGroupItem value="5">5</RadioGroupItem>
           </RadioGroup>
           <Button
-            onClick={() => PostNewReview(formData)}
             type="submit"
             className="rounded-[.25rem] w-full font-semibold"
           >
