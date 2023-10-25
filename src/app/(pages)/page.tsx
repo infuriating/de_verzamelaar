9ffba53fb35660979ccfb00ef37120d5230df448
inf <68154487+infuriating@users.noreply.github.com>
2023-10-25 11:29:07 +0200

    removed margins, made div not vertically centered on mobile

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-center h-minusheader flex gap-8 justify-center items-center flex-col md:grid md:grid-cols-2">
+    <div className="text-center h-minusheader flex gap-8 justify-center flex-col md:items-center md:grid md:grid-cols-2">
       <div>
         <p className="text-3xl font-medium">
           Welcome to <span className="font-bold">FNS Cameras</span>
         </p>
-        <p className="text-xl mt-4">
+        <p className="text-xl">
           Here at FNS Cameras we have an collection of old and new cameras.
           <br />
           Feel free to check out what we have in stock on our products page.
         </p>
         <Link href={"/products"}>
-          <Button className="mt-4 rounded-[.25rem] font-semibold">
+          <Button className="rounded-[.25rem] font-semibold">
             View Products
           </Button>
         </Link>
